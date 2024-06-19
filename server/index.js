d5c64b20b90e89268a4369c6a097218526d46d3b
OmkarDedge <omkardedge7@gmail.com>
2024-06-19 15:47:06 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+import Connection from "./database/db.js";
+import {
+  getDocument,
+  updateDocument,
+} from "./controllers/documentController.js";
+
+dotenv.config();
+
+const PORT = 9000;
+
+Connection();
+
+const io = new Server(PORT, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const doc = await getDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", doc.data);
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("recieve-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await updateDocument(documentId, data);
+    });
+  });
+});
