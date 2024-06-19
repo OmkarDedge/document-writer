b43b58fd1737f168b95b5f199783117614b97756
OmkarDedge <123588944+OmkarDedge@users.noreply.github.com>
2024-06-19 16:06:21 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ Connection();
 
 const io = new Server(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://document-writer-frontend.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
